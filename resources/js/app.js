5d3bd3406016e4bef2601be3664d29636efa4d57
Topan Sidiq <topansidiq28@gmail.com>
2025-07-14 21:11:34 +0700

    Install alpine dan lucide icon

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
+import Alpine from "alpinejs";
+import { createIcons, icons } from "lucide";
+
+window.Alpine = Alpine;
+Alpine.start();
+createIcons({ icons });
