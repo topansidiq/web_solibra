c89eb0b01bc9c33c37265557b1ccebbe497758d1
salsabilaagustinpy@gmail.com <126636885+SalsabilaAgustinPutriYendi@users.noreply.github.com>
2025-08-06 16:36:05 +0700

    perbaikan UI profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SOLIBRA - Perpustakaan Umum Kota Solok</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-</head>
+@section('title', 'Profil | Perpustakaan Umum Kota Solok')
 
-<body>
-    <x-header-guest />
+@section('content')
     <main>
         <div>
             <div class="w-full relative mx-auto overflow-hidden rounded-lg shadow-lg" x-data="carousel()"
@@ -47,114 +39,83 @@
                     </template>
                 </div>
             </div>
-
-
-
             <!-- Sambutan -->
-            <section class="py-10 px-6 w-full mx-auto bg-white">
-                <div class="bg-neutral-50 max-w-7xl mx-auto rounded-md shadow-md border border-gray-200 p-6">
-                    <h2
-                        class="text-left text-2xl font-bold pb-7 mb-3 border-b border-neutral-200 max-w-fit mx-auto text-neutral-700">
-                        Selamat
-                        Datang di
-                        Perpustakaan Umum Kota Solok
-                    </h2>
+            <section class="py-16 px-2 sm:px-4 lg:px-6 w-full mx-auto bg-white">
+                <div class="max-w-7xl mx-auto grid md:grid-cols-2 gap-8 lg:gap-12 items-center">
+                    <div>
+                        <h2 class="text-4xl font-semibold text-gray-900 mb-3 leading-snug">
+                            Selamat Datang di<br>
+                            <span class="text-sky-800">Perpustakaan Umum Kota Solok</span>
+                        </h2>
+                    </div>
 
-                    <div class="border-l-4 border-l-yellow-500 p-5">
-                        <p class="text-gray-700 leading-relaxed text-justify pl-1">
-                            <strong class="text-neutral-700">Perpustakaan Umum Kota Solok</strong> adalah salah satu
-                            lembaga
-                            penting
-                            di kota Solok, Sumatera
-                            Barat,
-                            yang berfungsi sebagai pusat pembelajaran dan pengembangan literasi di kalangan
-                            masyarakat.
-                            Dikenal dengan kota yang kaya akan budaya dan tradisi, Perpustakaan Kota Solok berperan
-                            besar
-                            dalam mendukung pembangunan pendidikan yang berkualitas, sekaligus memberikan akses bagi
-                            warga
-                            untuk memperoleh informasi dan pengetahuan yang mereka butuhkan. Sebagai lembaga
-                            pendidikan
-                            non-formal, perpustakaan ini terus berupaya untuk memperbaiki kualitas layanan dan
-                            meningkatkan
-                            minat baca masyarakat dari segala usia. Dalam artikel ini, kita akan membahas secara
-                            mendalam
-                            mengenai berbagai aspek yang ada di Perpustakaan Kota Solok, termasuk sejarah, visi dan
-                            misi,
-                            layanan, serta program-program unggulan yang mereka tawarkan.
+                    <div>
+                        <p class="text-gray-700 text-lg leading-relaxed mb-7">
+                            Perpustakaan Umum Kota Solok merupakan salah satu institusi penting di Kota Solok, Sumatera Barat, yang berperan sebagai pusat informasi, literasi, dan pembelajaran masyarakat. Dengan latar belakang budaya dan tradisi yang kaya, perpustakaan ini menjadi bagian dari upaya peningkatan kualitas pendidikan dan akses informasi yang inklusif bagi seluruh warga. Sebagai lembaga pendidikan non-formal, Perpustakaan Umum Kota Solok terus melakukan inovasi layanan guna menumbuhkan minat baca dan memperluas wawasan masyarakat dari berbagai kalangan. Dalam halaman ini, Anda dapat menjelajahi informasi lengkap terkait sejarah, visi dan misi, layanan yang tersedia, serta program-program unggulan yang dijalankan oleh perpustakaan ini.
                         </p>
                     </div>
                 </div>
             </section>
 
+            <!-- Sejahtera -->
+            <section class="py-16 px-2 sm:px-4 lg:px-6 w-full mx-auto bg-neutral-100">
+                <div class="max-w-7xl mx-auto grid md:grid-cols-2 gap-8 lg:gap-12 items-center">
+                    <div>
+                        <h2 class="text-4xl font-semibold text-gray-900 mb-3 leading-snug">
+                            Sejarah<br>
+                            <span class="text-sky-800">Perpustakaan Umum Kota Solok</span>
+                        </h2>
+                    </div>
 
-            <!-- Bacaan Anda -->
-            <section class="py-8 bg-gray-50">
-                <div class="max-w-5xl mx-auto px-6">
-                    <h3 class="text-xl font-semibold mb-4">Bacaan Anda</h3>
-                    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-                        {{-- @foreach ($books as $book)
-                    <div class="bg-white p-2 rounded shadow">
-                        <img src="{{ $book->cover }}" alt="{{ $book->title }}" class="w-full h-48 object-cover rounded">
-                        <h4 class="mt-2 text-sm font-semibold">{{ $book->title }}</h4>
-                        <p class="text-xs text-gray-500">Teknologi - {{ $book->published_at->format('d M Y') }}</p>
+                    <div class="text-gray-700 text-lg leading-relaxed space-y-4">
+                        <p>
+                            Dinas Perpustakaan dan Kearsipan Kota Solok awalnya bernama Kantor Arsip dan Perpustakaan Daerah atau disingkat dengan KAPD, yang didirikan pada tahun 2001 berdasarkan Peraturan Daerah Nomor 6. Kemudian dikuatkan dengan Peraturan Daerah Nomor 1 Tahun 2003 tentang bentuk dan susunan organisasi perangkat daerah dan Sekretariat DPRD kota Solok. Pada masa ini, Kantor Arsip dan Kantor Perpustakaan berada pada tempat yang berbeda.
+                        </p>
+                        <p>
+                            Pada tahun 2009 Kantor Arsip dan Perpustakaan Daerah berubah nama menjadi Kantor Arsip Dokumentasi dan Perpustakaan (KADP) Kota Solok berdasarkan Peraturan Daerah Kota Solok Nomor 18 Tahun 2009. Perubahan ini merupakan gabungan dari Kantor Perpustakaan Umum dan Kantor Arsip Daerah yang sebelumnya terpisah. Pada tahun 2016, berdasarkan Peraturan Daerah Nomor 5 Tahun 2016 Tentang Pembentukan Dan Susunan Perangkat Daerah, Kantor Arsip Dokumentasi dan Perpustakaan berubah nama menjadi Dinas Perpustakaan dan Kearsipan Kota Solok hingga saat ini.
+                        </p>
+                        <p>
+                            Berdasarkan Peraturan Walikota Solok Nomor 35 Tahun 2023 Tentang Kedudukan, Susunan Organisasi, Tugas dan Fungsi Serta Tata Kerja Dinas Perpustakaan dan Kearsipan pasal 2 ayat (1) menyebutkan bahwa Dinas merupakan unsur pelaksana urusan pemerintahan di bidang perpustakaan dan bidang kearsipan dipimpin oleh Kepala Dinas yang berkedudukan dan bertanggung jawab kepada Wali Kota melalui sekretaris Daerah.
+                        </p>
+                        <p>
+                            Dinas Perpustakaan dan Kearsipan Kota Solok saat ini  berada di Lingkungan Balaikota Solok Gedung G,  Jalan Lubuk Sikarah, Kelurahan IX Korong, Kecamatan Lubuk Sikarah, Kota Solok. Perpustakaan Umum Kota Solok saat ini berada di Jalan. Natsir Sutan Pamuncak, Kelurahan Simpang Rumbio, Kecamatan Lubuk Sikarah Kota Solok
+                        </p>
                     </div>
-                @endforeach --}}
-                    </div>
+
                 </div>
             </section>
 
-            <!-- Buku Favorit -->
-            <section class="py-8">
-                <div class="max-w-5xl mx-auto px-6">
-                    <h3 class="text-xl font-semibold mb-4">Buku Favorit</h3>
-                    <div class="grid grid-cols-2 md:grid-cols-6 gap-4">
-                        {{-- @foreach ($favoriteBooks as $book)
-                    <div class="bg-white p-2 rounded shadow">
-                        <img src="{{ $book->cover }}" alt="{{ $book->title }}" class="w-full h-48 object-cover rounded">
-                        <h4 class="mt-2 text-sm font-semibold">{{ $book->title }}</h4>
-                        <p class="text-xs text-gray-500">Teknologi - {{ $book->published_at->format('d M Y') }}</p>
+            {{-- Visi Misi --}}
+            <section class="py-16 px-2 sm:px-4 lg:px-6 w-full mx-auto bg-white">
+                <div class="max-w-7xl mx-auto grid md:grid-cols-2 gap-8 lg:gap-12 items-center">
+                    <div>
+                        <h2 class="text-4xl font-semibold text-gray-900 mb-3 leading-snug">
+                            Visi dan Misi<br>
+                            <span class="text-sky-800">Perpustakaan Umum Kota Solok</span>
+                        </h2>
                     </div>
-                @endforeach --}}
-                    </div>
-                </div>
-            </section>
 
-            <!-- Kegiatan Terbaru -->
-            <section class="py-8 bg-gray-50">
-                <div class="max-w-5xl mx-auto px-6">
-                    <h3 class="text-xl font-semibold mb-4">Kegiatan Terbaru</h3>
-                    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                        {{-- @foreach ($events as $event)
-                    <div class="bg-white p-4 rounded shadow">
-                        <img src="{{ $event->image }}" alt="{{ $event->title }}" class="w-full h-40 object-cover rounded mb-2">
-                        <h4 class="text-base font-bold">{{ $event->title }}</h4>
-                        <p class="text-sm text-gray-600 mb-2">{{ $event->excerpt }}</p>
-                        <a href="{{ route('event.show', $event->slug) }}" class="text-blue-600 text-sm">Baca Selengkapnya</a>
-                    </div>
-                @endforeach --}}
-                    </div>
-                </div>
-            </section>
-            <!-- Kegiatan Terbaru -->
-            <section class="py-8 bg-gray-50">
-                <div class="max-w-5xl mx-auto px-6">
-                    <h3 class="text-xl font-semibold mb-4">Visi dan Misi</h3>
-                    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, deleniti eveniet iure
-                            impedit
-                            recusandae alias, minima vel itaque suscipit pariatur perspiciatis eos cupiditate.
-                            Possimus
-                            corporis
-                            delectus ratione quibusdam voluptatem natus!</p>
+                    <div>
+                        <p class="text-gray-700 text-lg leading-relaxed mb-7">
+                            Visi dan Misi Dinas Perpustakaan dan Kearsipan Kota Solok dalam mewujudkan Visi dan Misi Pemerintah Kota Solok yaitu:
+                        </p>
+
+                        <p class="text-gray-700 text-lg leading-relaxed mb-7 italic font-semibold">
+                            “Terwujudnya Kota Solok Yang Diberkahi, Maju Dan Sejahtera Melalui Pengembangan Sektor Perdagangan Dan Jasa Yang Maju Modern”
+                        </p>
+
+                        <p class="text-gray-700 text-lg leading-relaxed mb-7">
+                            Dalam rangka mewujudkan visi sebagaimana yang tersebut di atas maka diperlukan sebuah Misi yang memuat rumusan mengenai upaya-upaya yang akan dilaksanakan dan diwujudkan agar tujuan dapat dilaksanakan dan berhasil dengan baik sesuai Visi, maka Misi Dinas Perpustakaan dan Kearsipan ialah:
+                        </p>
+
+                        <ul class="list-decimal ml-8 text-gray-700 text-lg leading-relaxed text-justify space-y-2 mb-7">
+                            <li>Peningkatan kualitas hidup masyarakat dengan meningkatkan derajat kesehatan, pendidikan dan perlindungan sosial.</li>
+                            <li>Peningkatan kapasitas pemerintahan dan manajemen birokrasi yang bersih, efektif dan efisien.</li>
+                        </ul>
                     </div>
                 </div>
             </section>
         </div>
     </main>
-    <x-footer-guest />
-
     <script src="{{ asset('js/guest/profile.js') }}"></script>
-</body>
-
-</html>
+@endsection
