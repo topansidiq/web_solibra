3515fbfbf90899cfadca288c9574be045ed60c5a
salsabilaagustinpy@gmail.com <126636885+SalsabilaAgustinPutriYendi@users.noreply.github.com>
2025-08-19 15:21:16 +0700

    Internationalization of Information

---
+++ resources/lang/en/information.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'activity_information' => 'Activity Information',
+    'latest_activities' => 'Latest activities from the Solok Public Library',
+    'view_more' => 'See More →',
+    'filter_information' => 'Filter Information',
+    'upcoming' => 'Upcoming',
+    'ongoing' => 'Ongoing',
+    'completed' => 'Completed',
+    'cancelled' => 'Cancelled',
+    'load_more' => 'Load More',
+    'load_less' => 'Load Less',
+];
---
+++ resources/lang/id/information.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'activity_information' => 'Informasi Kegiatan',
+    'latest_activities' => 'Kegiatan terbaru dari Perpustakaan Umum Kota Solok',
+    'view_more' => 'Lihat Selengkapnya →',
+    'filter_information' => 'Filter Informasi',
+    'upcoming' => 'Akan Datang',
+    'ongoing' => 'Sedang Berlangsung',
+    'completed' => 'Sudah Selesai',
+    'cancelled' => 'Dibatalkan',
+    'load_more' => 'Lihat Lebih Banyak',
+    'load_less' => 'Lihat Lebih Sedikit',
+];
